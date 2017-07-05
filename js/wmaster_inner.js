051683967da6d857a8b6d8475071801127dd0f5c
William P. French <williamfrench4@gmail.com>
2017-07-04 20:03:41 -0400

    data

--- js/wmaster_inner.js
+++ js/wmaster_inner.js
@@ -578,6 +578,7 @@ jQuery(function () {
       count_words: {append: '.article-timestamp', subject: '.article-content'},
       article_hide_selector: '.clay-share, .page-header',
       article_theme_foreground_selector: 'h1, p, figcaption, time, .by-authors',
+      article_theme_background_selector: '.video-promo-for-articles',
       article_css: 'blockquote, blockquote p {' + theme_foreground_rule + 'padding: 0 0 0 14px; border-left: 1px solid #3a3} blockquote:before {border-top: none} .mediaplay-image-figcaption {text-align: left} ' +
         'nav.rubric-wrap svg>path:last-child {fill: white}', // "Intelligencer" in Daily Intelligencer logo
       homepage_theme_background_selector: '.newsHeadlinesByPublication section, .dek, .hed',
@@ -737,6 +738,7 @@ jQuery(function () {
       article_css: '.trb_mh {margin-top: 70px} a:link[   href^="/topic/"] {color: #00e766} a:link:hover[href^="/topic/"] {color: #00f} a:visited[href^="/topic/"] {color: #99d700} a:visited:hover[href^="/topic/"] {color: purple}' ,
       hide_selector: '.met-promo',
       count_words: {append: '.trb_ar_dateline', subject: 'div[itemprop="articleBody"]', nbsp_size: '100%'},
+      unwanted_query_fields: 'bn',
       customize() {
         const chunks = jQuery('div[itemprop="articleBody"] p');
         //console.log(41, chunks);
