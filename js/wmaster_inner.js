9a287e7125e784ef4dccd78c1fd2a1b0bd57d8ad
William P. French <williamfrench4@gmail.com>
2019-08-24 01:58:32 -0400

    data

--- js/wmaster_inner.js
+++ js/wmaster_inner.js
@@ -799,7 +799,7 @@ const sites_data = [
     article_css: 'html, .u-overflowHidden {overflow: auto} .u-fixed, .metabar--affixed {position: static}',
     article_theme_background_selector: '#container, .screenContent, .canvas-renderer, .metabar, .u-backgroundGrayLightest, .u-backgroundWhite',
     article_theme_foreground_selector: '.postMetaInline, .u-textColorDark, .graf--h2, .graf--h3, .graf--h4',
-    article_hide_selector: 'body>iframe, .overlay, .surface-scrollOverlay, .postActions',
+    article_hide_selector: 'body>iframe, .overlay, .surface-scrollOverlay, .postActions, div.ar',
   },
   {
     name: 'Codeburst',
