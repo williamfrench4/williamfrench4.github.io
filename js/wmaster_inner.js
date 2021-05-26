ddcf123ceb998879cd124f417566c84f3862cf16
William P. French <williamfrench4@gmail.com>
2021-05-26 17:31:33 -0400

    data

--- js/wmaster_inner.js
+++ js/wmaster_inner.js
@@ -827,7 +827,7 @@ const sites_data = [
     article_hide_selector: 'nav, #paywall, gpt-ad, .ad-boxinjector-wrapper, .fancy-box-wrap, c-non-metered-nudge, .module-related.video, .js-inject-promo, .social-kit-top, .article-tools, .callout, .pullquote, .c-nudge__container,' + selector_for_elements_with_a_class_that_starts_with('NonMeteredNudge_root__ adform-adbox- GptAd_'),
     theme_background_selector: '#site',
     article_theme_background_selector: '.article-body blockquote',
-    article_theme_foreground_selector: '.article-body',
+    article_theme_foreground_selector: '.ArticleBody_root__17rER, .ArticleTitle_root__1SxDD, .ArticleDek_root__1_tnX, .article-body',
     homepage_theme_background_selector: '.c-hp, .c-hp-lead__content, .c-hp-news, .c-hp-filmstrip, .c-hp-offlead, .c-hp-featured, .c-writers__container, .c-latest, .c-popular__container, .c-sections, input',
     homepage_hide_selector: '.c-writers__item--magazine',
     article_css: 'html {scroll-padding-top: 0} .fancybox-lock, .fancybox-lock body, body {overflow: scroll; position: static} figure.lead-img .img {outline: none} .article-cover-extra {padding-bottom: 0; border-bottom: none}',
